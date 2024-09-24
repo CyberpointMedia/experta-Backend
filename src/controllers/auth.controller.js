0cc4413dad2c4df79ea7019e7c61dbeca9228f9b
ManshuSengar <89942713+ManshuSengar@users.noreply.github.com>
2024-09-24 14:44:53 +0530

    added in api check token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,11 +4,16 @@ const globalConstants = require("../constants/global-constants");
 const jwtUtil = require("../utils/jwt.utils");
 
 const authService = require("../services/auth.service");
+const User = require("../models/user.model");
 // const userService = require("../services/user.service");
 
 const createResponse = require("../utils/response");
-
-const { AuthenticationError,ValidationError } = require("../errors/custom.error");
+var jwt = require("jsonwebtoken");
+const config = require("../config/config");
+const {
+  AuthenticationError,
+  ValidationError,
+} = require("../errors/custom.error");
 
 exports.handleLogin = async (req, res) => {
   if (!req.body.phoneNo || "" == req.body.phoneNo) {
@@ -228,5 +233,68 @@ exports.verifyOtpAndChangeEmail = async (req, res) => {
         })
       );
     }
+  }
+};
+
+exports.checkTokenValidity = (req, res) => {
+  if (
+    !req.headers.authorization ||
+    "" == req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer")
+  ) {
+    res.status(403);
+    res.send(
+      createResponse.unauthorized(
+        globalConstants.TOKEN_ISSUE_CODE,
+        "You are unauthorized.",
+        "Not a valid token or token is empty."
+      )
+    );
+    return;
+  }
+  try {
+    let authToken = req.headers.authorization.split(" ");
+    jwt.verify(authToken[1], config.jwt.secret, async function (err, decoded) {
+      if (err) {
+        res.status(403);
+        res.send(
+          createResponse.unauthorized(
+            globalConstants.TOKEN_ISSUE_CODE,
+            "You are unauthorized.",
+            "Not a valid token or token is expired."
+          )
+        );
+        return;
+      }
+      let user = await User.findById(decoded._id);
+      if (
+        !user ||
+        (decoded.phoneNo && user.phoneNo != decoded.phoneNo) ||
+        (decoded.id && user.id != decoded.id)
+      ) {
+        res.status(403);
+        res.send(
+          createResponse.unauthorized(
+            globalConstants.TOKEN_ISSUE_CODE,
+            "You are unauthorized.",
+            "Invalid user or user does not exist."
+          )
+        );
+        return;
+      }
+       res.json(decoded);
+       return;
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(403);
+    res.send(
+      createResponse.unauthorized(
+        globalConstants.TOKEN_ISSUE_CODE,
+        "You are unauthorized.",
+        e.message
+      )
+    );
+    return;
   }
 };
