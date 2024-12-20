b02005ec44d1561d8d898dec7275fe23d60e8ec2
Laxman Gautam <laxman@cyberpointmedia.com>
2024-12-20 13:01:38 +0530

    Register all server resource's routes in centralize server module

---
+++ api/routes/index.js
@@ -0,0 +1,16 @@
+/**
+ * Module: Api Routes
+ * Info: Register all api routes for handle incoming requests
+ **/
+
+// Import Module dependencies.
+const express = require("express");
+const apiRouter = express.Router();
+
+/**
+ * Register all routes for api resources.
+ */
+//apiRouter.use("/", userRoutes);
+
+const registerApiRoutes = apiRouter;
+module.exports = registerApiRoutes;
