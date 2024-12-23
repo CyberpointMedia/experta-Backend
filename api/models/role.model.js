648e3791b58fa3451a6de59a8d61d85bcaa93174
ekanshcyberpoint <156168640+ekanshcyberpoint@users.noreply.github.com>
2024-12-23 13:41:49 +0530

    CHnages in roles schema

--- api/models/role.model.js
+++ api/models/role.model.js
@@ -3,33 +3,40 @@ const roleEnum = require("../enums/role.enum");
 
 const roleSchema = new mongoose.Schema({
 
-_id:{
-    type:String,
-    unique:true,
-    required:true,
-    uppercase:true,
-    maxlength:10,
-    trim:true,
-    match: [/^[a-zA-Z]+$/, "It allows only characters: a-zA-Z"],
-},
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    enum: Object.values(roleEnum),
-  },
-  permissions:
-    [{
-      type: String,
-      ref: "Permission"
-    }],
-  description: {
-    type: String,
-    trim:true,
-  },
-}, { timestamps: true, 
+    _id: {
+        type: String,
+        unique: true,
+        required: true,
+        uppercase: true,
+        maxlength: 10,
+        trim: true,
+        match: [/^[a-zA-Z]+$/, "It allows only characters: a-zA-Z"],
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        enum: Object.values(roleEnum),
+    },
+    displayName: {
+        type: String,
+        required: false,
+        unique: true,
+        trim: true,
+    },
+    permissions:
+        [{
+            type: String,
+            ref: "Permission"
+        }],
+    description: {
+        type: String,
+        trim: true,
+    },
+}, {
+    timestamps: true,
     toJSON: { virtuals: true },
-    toObject: { virtuals: true } 
+    toObject: { virtuals: true }
 });
 
 module.exports = mongoose.model("Role", roleSchema);
