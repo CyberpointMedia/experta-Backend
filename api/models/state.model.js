008d928c5a178911187affb46d028cbb2375aa19
Laxman Gautam <laxman@cyberpointmedia.com>
2024-12-27 15:13:19 +0530

    Add state model for manage states

---
+++ api/models/state.model.js
@@ -0,0 +1,35 @@
+/**
+ * Module: Model
+ * Info:
+ **/
+
+// Import Module dependencies.
+const { Schema, model } = require("mongoose");
+const SchemaComposePlugin = require("./plugins/schemaComposer");
+const ModelName = "State";
+
+// Define Model Schema rules and options
+const excludeOptions = {
+  trackCreatedAt: true,
+  trackUpdatedAt: true,
+  trackDeletedAt: true,
+};
+const schemaRules = {
+  name: { type: String, required: true, unique: true }, // Full name of the state
+  code: { type: String, required: true, unique: true, maxlength: 2 }, // 2-letter abbreviation
+  countryCode: {
+    type: String,
+    default: "IN",
+  },
+};
+
+//Compose Model Schema
+const ModelSchema = new Schema(schemaRules);
+
+// Apply the common properties plugin to the Post schema
+ModelSchema.plugin(SchemaComposePlugin, excludeOptions);
+
+//Create model
+const Model = model(ModelName, ModelSchema);
+
+module.exports = Model;
