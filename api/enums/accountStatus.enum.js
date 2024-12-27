ba542eb74716d746e4a63853cff11e1fd68a6773
Laxman Gautam <laxman@cyberpointmedia.com>
2024-12-27 07:04:32 +0530

    Account Status enum with methods

---
+++ api/enums/accountStatus.enum.js
@@ -0,0 +1,32 @@
+/**
+ * Module: AccountStatus Enum
+ * Info: Manage codes for Account status
+ **/
+
+const AccountStatus = Object.freeze({
+  statusCodes: {
+    PENDING:
+      "The account is pending verification. Please complete the process to activate your account.",
+    VERIFIED: "The account is verified and online.",
+    SUSPENDED:
+      "The account has been suspended due to a policy violation. Please contact support.",
+    BANNED: "account has been banned permanently. Please contact support.",
+    DELETED: "The account is marked for deletion, and the process is underway.",
+  },
+
+  // Method to get all keys
+  getAllStatus() {
+    return Object.keys(this.statusCodes);
+  },
+
+  // Method to get message by status
+  async getStatusMessage(CODE) {
+    if (this.statusCodes[CODE]) {
+      return this.statusCodes[CODE];
+    } else {
+      throw new TypeError(`Invalid account status code : ${CODE}`);
+    }
+  },
+});
+
+module.exports = AccountStatus;
