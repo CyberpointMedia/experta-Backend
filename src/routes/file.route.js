ea3e57607072fa73ffccad5c31619c60c010b7f1
ekanshcyberpoint <156168640+ekanshcyberpoint@users.noreply.github.com>
2025-01-03 12:22:51 +0530

    add auth in the video uplaod api

--- src/routes/file.route.js
+++ src/routes/file.route.js
@@ -1,12 +1,13 @@
 
 const fileUploadController = require("../controllers/file.controller");
 const routes = require("../constants/route.url");
+const { authMiddleware } = require("../middlewares/auth.middleware");
 
 module.exports = (app) => {
   var router = require("express").Router();
   router.post("/upload", fileUploadController.uploadFile);
   router.delete("/delete-file", fileUploadController.deleteFile);
-  router.get("/videos", fileUploadController.getVideoFiles);
-  router.post("/uploadRecordingVideo", fileUploadController.uploadRecordingVideo); 
+  router.get("/videos",authMiddleware, fileUploadController.getVideoFiles);
+  router.post("/uploadRecordingVideo",authMiddleware, fileUploadController.uploadRecordingVideo); 
   app.use(routes.API, router);
 };
