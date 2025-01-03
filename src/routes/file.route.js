61b2938bf44b1eccfbed0954cdd623427378e38c
ekanshcyberpoint <156168640+ekanshcyberpoint@users.noreply.github.com>
2025-01-03 11:54:25 +0530

    disputevideo api

--- src/routes/file.route.js
+++ src/routes/file.route.js
@@ -6,5 +6,7 @@ module.exports = (app) => {
   var router = require("express").Router();
   router.post("/upload", fileUploadController.uploadFile);
   router.delete("/delete-file", fileUploadController.deleteFile);
+  router.get("/videos", fileUploadController.getVideoFiles);
+  router.post("/uploadRecordingVideo", fileUploadController.uploadRecordingVideo); 
   app.use(routes.API, router);
 };
